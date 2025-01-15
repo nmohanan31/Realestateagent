b82ce9e9c031e7d12cb2eb134c51b0dda8859388
naveen.mohanan <naveen.mohanan@gmail.com>
2025-01-15 19:36:45 +0100

    Cleaned code added comments and added sample output for check

--- userinterface_and_search.py
+++ userinterface_and_search.py
@@ -1,34 +1,16 @@
-import os
-import csv
 import logging
-import re
-import numpy as np
 import lancedb
+import os
+import numpy as np
 import datetime
 
-from typing import Any, Dict, List
 from sentence_transformers import SentenceTransformer
 
-# Setup logging
+
+
+# Set up logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-
-# Define preference questions and answers
-preference_questions = [
-    "How big do you want your house to be?",
-    "What are 3 most important things for you in choosing this property?",
-    "Which amenities would you like?",
-    "Which transportation options are important to you?",
-    "How urban do you want your neighborhood to be?"
-]
-
-preference_answers = [
-    "A comfortable three-bedroom house with a spacious kitchen and a cozy living room.",
-    "A quiet neighborhood, good local schools, and convenient shopping options.",
-    "A backyard for gardening, a two-car garage, and a modern, energy-efficient heating system.",
-    "Easy access to a reliable bus line, proximity to a major highway, and bike-friendly roads.",
-    "A balance between suburban tranquility and access to urban amenities like restaurants and theaters."
-]
 
 def initialize_database():
     """Initialize database connection"""
@@ -192,8 +174,6 @@ def similarity_search_store(sorted_properties):
         "top_recommendations",
         data=[{
             'id': prop_id,
-            'preference_questions': preference_questions,
-            'preference_answers': preference_answers,
             'description': prop_data['details']['description'],
             'price': prop_data['details']['price'],
             'location': prop_data['details']['location'],
